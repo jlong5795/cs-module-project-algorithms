657166c21b516b710996806ddbe15f077bfa2e1d
Jason <jlong5795@gmail.com>
2020-05-20 15:30:04 -0500

    initial commit

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,18 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # iterate through the array looking at each value
+    for each in arr:
+        # evaluate if the value is a non-zero value
+        if each > 0:
+            # save the value
+            value = arr[each]
+            # pop it out of the array
+            arr.remove(arr[each])
+            # insert it at the beginning
+            arr.insert(0, value)
+    print(arr)
+    return arr
 
 
 if __name__ == '__main__':
