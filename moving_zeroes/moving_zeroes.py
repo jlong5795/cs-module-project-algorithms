4f29adb7c0bb5e7636d11a8ddeec80adce515e56
Jason <jlong5795@gmail.com>
2020-05-20 15:56:48 -0500

    halfway there

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -6,13 +6,11 @@ def moving_zeroes(arr):
     # iterate through the array looking at each value
     for each in arr:
         # evaluate if the value is a non-zero value
-        if each > 0:
-            # save the value
-            value = arr[each]
+        if each == 0:
             # pop it out of the array
-            arr.remove(arr[each])
+            arr.remove(each)
             # insert it at the beginning
-            arr.insert(0, value)
+            arr.append(0)
     print(arr)
     return arr
 
@@ -20,5 +18,5 @@ def moving_zeroes(arr):
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
-
+    moving_zeroes(arr)
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
